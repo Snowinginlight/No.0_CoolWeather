77860564bd9f53c8977940691b3e3d349e673d42
xuedou <xuedouwuzhi@163.com>
2016-10-19 14:09:54 +0800

    新增数据库帮助类、以及各表对应实体类

---
+++ app/src/main/java/com/coolweather/app/db/CoolWeatherOpenHelper.java
@@ -0,0 +1,42 @@
+package com.coolweather.app.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by 无知 on 2016/10/19.
+ */
+
+public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
+
+    public static final String CREATE_PROVINCE = "create table Probince("
+                                                  + "id integer primary key autoincrement,"
+                                                  + "province_name text,"
+                                                  + "province_code text)";//province表建表语句
+    public static final String CREATE_CITY = "create table City("
+                                              + "id integer primary key autoincrement,"
+                                              + "city_name text,"
+                                              + "city_code text,"
+                                              +"province_id integer)";//city表建表语句
+    public static final String CREATE_COUNTY = "create table County("
+                                                + "id integer primary key autoincrement,"
+                                                + "county_name text,"
+                                                + "county_code text,"
+                                                +"city_id integer)";//county表建表语句
+    public CoolWeatherOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_PROVINCE);//建表
+        db.execSQL(CREATE_CITY);
+        db.execSQL(CREATE_COUNTY);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
